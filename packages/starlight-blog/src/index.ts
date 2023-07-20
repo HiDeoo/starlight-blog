01d9c72e1673cdc96674b56632405099e517f304
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-07-20 10:45:34 +0200

    refactor: component aliases

--- packages/starlight-blog/src/index.ts
+++ packages/starlight-blog/src/index.ts
@@ -1,6 +1,6 @@
-import path from 'node:path'
+import type { AstroIntegration, AstroUserConfig } from 'astro'
 
-import type { AstroIntegration, AstroUserConfig, ViteUserConfig } from 'astro'
+import { vitePluginStarlightBlog } from './libs/vite'
 
 export default function starlightBlogIntegration(): AstroIntegration {
   return {
@@ -15,41 +15,6 @@ export default function starlightBlogIntegration(): AstroIntegration {
 
         updateConfig(config)
       },
-    },
-  }
-}
-
-const starlightComponentOverrides = {
-  Sidebar: 'BlogSidebar',
-  SocialIcons: 'BlogSocialIcons',
-}
-
-const overriddenStarlightComponents = Object.keys(
-  starlightComponentOverrides,
-) as (keyof typeof starlightComponentOverrides)[]
-
-function vitePluginStarlightBlog(): NonNullable<ViteUserConfig['plugins']>[number] {
-  return {
-    enforce: 'pre',
-    name: 'vite-plugin-starlight-blog',
-    resolveId(source, importer) {
-      if (source.startsWith('/node_modules')) {
-        return
-      }
-
-      for (const component of overriddenStarlightComponents) {
-        if (source.endsWith(`/${component}.astro`)) {
-          if (importer?.includes('node_modules')) {
-            return path.resolve(
-              `node_modules/starlight-blog/src/components/${starlightComponentOverrides[component]}.astro`,
-            )
-          }
-
-          return path.resolve(`node_modules/@astrojs/starlight/components/${component}.astro`)
-        }
-      }
-
-      return
     },
   }
 }
---
+++ packages/starlight-blog/src/libs/vite.ts
@@ -0,0 +1,47 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import type { ViteUserConfig } from 'astro'
+
+const componentAliases = ['Sidebar', 'SocialIcons'] as const
+
+const aliasIds = new Map<ComponentAlias, string>()
+
+// Alias various starlight components to local ones.
+// The local components should be located in the `src/components` directory, use the same name as the aliased component,
+// and be prefixed with an underscore.
+export function vitePluginStarlightBlog(): VitePlugin {
+  return {
+    enforce: 'pre',
+    name: 'vite-plugin-starlight-blog',
+    async resolveId(source, importer, options) {
+      if (source.startsWith('/@fs')) {
+        return
+      }
+
+      for (const alias of componentAliases) {
+        if (source.endsWith(`/${alias}.astro`)) {
+          // If the component is imported by starlight, use the aliased component.
+          if (!importer?.includes('starlight-blog/src/components')) {
+            const resolvedAlias = await this.resolve(source, importer, { ...options, skipSelf: true })
+
+            if (resolvedAlias) {
+              aliasIds.set(alias, resolvedAlias.id)
+            }
+
+            return path.join(path.dirname(fileURLToPath(import.meta.url)), `../components/_${alias}.astro`)
+          }
+
+          // If the component is imported internally by starlight-blog, use the original component.
+          return aliasIds.get(alias)
+        }
+      }
+
+      return
+    },
+  }
+}
+
+type ComponentAlias = (typeof componentAliases)[number]
+
+type VitePlugin = NonNullable<ViteUserConfig['plugins']>[number]
