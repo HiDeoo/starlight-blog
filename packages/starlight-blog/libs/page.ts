6d893e04d6c7a87fbd241904e0dc3e21d8cb9719
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-03-10 09:52:58 +0100

    fix: fixes issues when using the Astro `base` option

--- packages/starlight-blog/libs/page.ts
+++ packages/starlight-blog/libs/page.ts
@@ -1,3 +1,11 @@
+const base = stripTrailingSlash(import.meta.env.BASE_URL)
+
+export function getPathWithBase(path: string) {
+  path = stripLeadingSlash(path)
+
+  return path ? `${base}/${path}` : `${base}/`
+}
+
 export function isAnyBlogPage(slug: string) {
   return slug.match(/^blog(\/?$|\/.+\/?$)/) !== null
 }
@@ -24,6 +32,22 @@ export function getPageProps(title: string): StarlightPageProps {
       title,
     },
   }
+}
+
+function stripLeadingSlash(path: string) {
+  if (!path.startsWith('/')) {
+    return path
+  }
+
+  return path.slice(1)
+}
+
+function stripTrailingSlash(path: string) {
+  if (!path.endsWith('/')) {
+    return path
+  }
+
+  return path.slice(0, -1)
 }
 
 interface StarlightPageProps {
