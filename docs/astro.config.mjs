b3c76fb46633b6eb3f6bdc2caeb017b09958cfca
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-06-24 16:56:45 +0200

    docs: add showcase

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -40,7 +40,10 @@ export default defineConfig({
         },
         {
           label: 'Resources',
-          items: [{ label: 'Plugins and Tools', link: '/resources/starlight/' }],
+          items: [
+            { label: 'Showcase', link: '/resources/showcase/' },
+            { label: 'Plugins and Tools', link: '/resources/starlight/' },
+          ],
         },
       ],
       social: {
