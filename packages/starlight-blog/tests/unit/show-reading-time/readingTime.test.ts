79b0ffe191489fdade5c014f92f551aeb67415c5
trueberryless <99918022+trueberryless@users.noreply.github.com>
2024-11-11 17:27:42 +0100

    Update readingTime.test.ts

--- packages/starlight-blog/tests/unit/show-reading-time/readingTime.test.ts
+++ packages/starlight-blog/tests/unit/show-reading-time/readingTime.test.ts
@@ -1,15 +1,21 @@
 import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest'
 
 import { calculateReadingTime, getReadingTime, formatReadingTime } from '../../../libs/readingTime'
+import { mockBlogPost } from '../utils'
 
 describe('getReadingTime', () => {
+  const postWithoutTime = mockBlogPost(
+    'post-1.md', { title: 'Home Page', date: new Date('2023-08-24') }
+  )
+  const postWithTime = mockBlogPost(
+    'post-2.md', { title: 'Home Page', date: new Date('2023-08-25'), readingTime: 12 }
+  )
+
   test('show reading time, overridden by frontmatter', () => {
-    expect(getReadingTime()).toBe(1)
-    expect(getReadingTime()).toBe(1)
+    expect(getReadingTime(postWithTime)).toBe({ showReadingTime: true, readingTime: 0 })
   })
 
   test('show reading time, calculated automatically', () => {
-    expect(getReadingTime()).toBe(1)
-    expect(getReadingTime()).toBe(1)
+    expect(getReadingTime(postWithoutTime)).toBe({ showReadingTime: true, readingTime: 12 })
   })
 })
